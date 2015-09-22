e002bc7e0e36d342cddbc5fa2336b85b64ed823d
dgcollier <david@gcollier.me>
2015-09-22 15:29:34 -0500

    added data to Parameters seeder

--- app/database/seeds/ParametersTableSeeder.php
+++ app/database/seeds/ParametersTableSeeder.php
@@ -5,15 +5,16 @@ class ParametersTableSeeder extends Seeder {
 	public function run()
 	{
 		$parameter = new Parameter();
-        $parameter->review_id       = ;
-        $parameter->grind           = ;
-        $parameter->water_weight    = ;
-        $parameter->coffee_weight   = ;
-        $parameter->brew_time       = ;
-        $parameter->water_temp      = ;
-        $parameter->brewer          = ;
-        $parameter->method          = ;
-        $parameter->roast_date      = ;
+                $parameter->review_id       = 1;
+                $parameter->grind           = 'medium burr';
+                $parameter->water_weight    = 532;
+                $parameter->coffee_weight   = 33.2;
+                $parameter->brew_time       = 240;
+                $parameter->water_temp      = 208;
+                $parameter->brewer          = 'Chemex';
+                $parameter->method          = '30-second bloom, 16:1 ratio';
+                $parameter->roast_date      = '09/10/2015';
+                $parameter->save();
 	}
 
 }
