cb437883c8eeb8e9cb250ec0c77bc5200bb9b211
j-womack <womack.joshua@gmail.com>
2015-09-29 15:25:36 -0500

    removed unused package

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,6 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Mitch\Hashids\HashidsServiceProvider',
-		'Sajjadrad\Invi\InviServiceProvider',
 
 	),
 
