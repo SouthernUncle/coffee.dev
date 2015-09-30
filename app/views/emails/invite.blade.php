2e6814a1199354af370d0ab2559f8455367d6042
dgcollier <david@gcollier.me>
2015-09-30 10:06:05 -0500

    adding auth filters to controllers. mid-way through that. invite is auth-ed and visiting invite.create nulls out invite code in DB.

--- app/views/emails/invite.blade.php
+++ app/views/emails/invite.blade.php
@@ -1,8 +1,10 @@
-Hey {{ $name }},
+Hey {{ $name }},<br><br>
 
 Somebody you know thinks that you'd be a great contributor to ........
-To accept their invitation and create an account, click this link
+To accept their invitation and create an account, click this link: <br><br>
 
-<a href="coffee.dev/createnewuser/{{ $confirmation }}">Create an Account!</a>
+<a href="http://coffee.dev/createnewuser/{{ $confirmation }}">Create an Account!</a><br><br>
+
+If you haven't checked us out yet, do so first at <a href="http://coffee.dev">coffee.dev</a>!<br><br>
 
 Thanks!
