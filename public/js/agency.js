c1c1cdb5209b021ef3f785887c128fd0a7ac1a72
dgcollier <david@gcollier.me>
2015-10-03 16:01:45 -0500

    fixed navbar on mobile (dropdowns). fixed regions index (text over image) on mobile

--- public/js/agency.js
+++ public/js/agency.js
@@ -19,8 +19,3 @@ $(function() {
 $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
