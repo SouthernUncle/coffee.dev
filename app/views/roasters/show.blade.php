51eb514ebb5c25aa5345f47d03b1d7e4cd63b74c
dgcollier <david@gcollier.me>
2015-10-04 14:31:30 -0500

    added edit button on roaster show to link for admin only to roaster edit page.

--- app/views/roasters/show.blade.php
+++ app/views/roasters/show.blade.php
@@ -7,24 +7,35 @@
         <h1>{{ $roaster->name }}</h1>
         <h2>{{ $roaster->overallRoasterScore() }} / 100</h2>
         <p>{{ $roaster->description }}</p>
-        <img src="/img/fit750{{ $roaster->img_url }}" class="img-responsive">
+        <img src="/img/fit750{{ $roaster->img_url }}" class="img-responsive"><br>
+
+        @if(Auth::user()->role_id == 1)
+            <a href="{{{ action('RoastersController@edit', $roaster->id) }}}">
+                <button class="btn btn-info">Edit</button>
+            </a>
+        @endif
+
         <table class="table table-responsive">
-            <th>Coffee</th>
-            <th>Region</th>
-            <th>Overall Rating</th>
-            <th>Number of Reviews</th>
-        @foreach ($coffees as $c)
-        <tr>
-            <td>{{ HTML::linkAction('CoffeesController@show', $c->name, array($c->id)) }}</td>
-            <td>{{ HTML::linkAction('RegionsController@show', $c->region->name, array($c->region->id)) }}</td>
-            @if(isset($c->reviews[0]))
-                <td>{{ $c->overallCoffeeRating() }}</td>
-            @else
-                <td>Not Yet Rated</td>
-            @endif
-            <td>{{ $c->reviews->count() }}</td>
-        </tr>
-        @endforeach
+            <thead>
+                <th>Coffee</th>
+                <th>Region</th>
+                <th>Overall Rating</th>
+                <th>Number of Reviews</th>
+            </thead>
+            <tbody>
+                @foreach ($coffees as $c)
+                    <tr>
+                        <td>{{ HTML::linkAction('CoffeesController@show', $c->name, array($c->id)) }}</td>
+                        <td>{{ HTML::linkAction('RegionsController@show', $c->region->name, array($c->region->id)) }}</td>
+                        @if(isset($c->reviews[0]))
+                            <td>{{ $c->overallCoffeeRating() }}</td>
+                        @else
+                            <td>Not Yet Rated</td>
+                        @endif
+                        <td>{{ $c->reviews->count() }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
         <div>{{ $coffees->links() }}</div>
     </div>
