d23bb23311a79100b9969734ecc03d6cd20237ca
dgcollier <david@gcollier.me>
2015-10-05 00:07:25 -0500

    styled login page. added nav highlights for roasters create/edit

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,19 +5,25 @@
 @section('content')
 	<div class="container">
         {{ Form::open(array('action' => 'HomeController@doLogin', 'class' => 'form-horizontal', 'id' => 'login')) }}
-            <div class="col-xs-12 col-md-12">
+            <div class="col-xs-12 col-s-12 col-md-12">
+            	<div class="col-s-6 col-md-6">
             	{{ Form::label('username', 'Username', ['class' => 'fancy']) }}
-				{{ Form::text('username', null, [ 'autofocus', 'class' => 'form-control']) }}
+				{{ Form::text('username', null, [ 'autofocus', 'class' => 'form-control brown']) }}
+				</div>
 			</div>
 
-			<div class="col-xs-12 col-md-12">
+			<div class="col-xs-12 col-s-12 col-md-12">
+				<div class="col-s-6 col-md-6">
 				{{ Form::label('password', 'Password', ['class' => 'fancy']) }}
-				{{ Form::password('password', ['class' => 'form-control']) }}
+				{{ Form::password('password', ['class' => 'form-control brown']) }}
+				</div>
 
             </div>
 
             <div class="col-xs-12 col-md-12">
-	            <button class="btn btn-default">Log In</button>
+            	<div class ="col-xs-12 col-s-12 col-md-12">
+		            <button class="btn btn-info">Log In</button>
+	            </div>
             </div>
         {{ Form::close() }}
 	</div>
