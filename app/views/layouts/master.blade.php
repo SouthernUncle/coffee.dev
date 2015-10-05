a386f8f9ff2f574dbe734c723cff5ac850e47706
dgcollier <david@gcollier.me>
2015-10-04 23:58:59 -0500

    fixed sticky footer

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,19 +3,17 @@
 <head>
     @include('layouts.partials.head')
     <link rel="stylesheet" type="text/css" href="/css/body.css">
+    <link rel="stylesheet" type="text/css" href="/css/sticky-footer.css">
 </head>
-
-
 
 <header>
 	@include('layouts.partials.nav')
 </header>
 
 <body id="page-top" class="index" data-spy="scroll" data-target=".navbar" data-offset="50">
-<div class="wrapper">
+
 	@include('layouts.partials.main_errors')
-	<div class="push"></div>
-</div>
+
 	@include('layouts.partials.footer')
 
 	@include('layouts.partials.script')
--- app/views/layouts/partials/footer.blade.php
+++ app/views/layouts/partials/footer.blade.php
@@ -1,17 +1,14 @@
-
 <footer class="footer-container footer">
     <hr class="featurette-divider">
-    {{-- <div class="row"> --}}
-        <div class="col-xs-4">
-            <a href="#" class="squeeze text-ccenter">
-            <i class="fa fa-angle-up"></i> Back to Top</a>
-        </div>
-        <div class="col-xs-4">
-            <i class="fa fa-copyright"></i> 2015 J. Womack & D. Collier</a>
-        </div>
-        <div class="col-xs-4">
-            <a href="https://github.com/SouthernUncle/coffee.dev/" target="_blank" class="squeeze text-ccenter">
-            <i class="fa fa-github"></i> Southern Uncle</a>
-        </div>
-    {{-- </div> --}}
+    <div class="col-xs-4">
+        <a href="#" class="squeeze text-ccenter">
+        <i class="fa fa-angle-up"></i> Back to Top</a>
+    </div>
+    <div class="col-xs-4">
+        <i class="fa fa-copyright"></i> 2015 J. Womack & D. Collier</a>
+    </div>
+    <div class="col-xs-4">
+        <a href="https://github.com/SouthernUncle/coffee.dev/" target="_blank" class="squeeze text-ccenter">
+        <i class="fa fa-github"></i> Southern Uncle</a>
+    </div>
 </footer>
